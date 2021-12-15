c71869fab991bd291ffb8b39f061b701384e8632
billy <Billy@mask.io>
2021-12-16 02:16:00 +0800

    feat: news

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="box">
     <div class="left">
-      <img src="../assets/images/rect.jpg" alt="rect"/>
+      <img src="../assets/images/rect.jpg" alt="rect" />
       <div class="leftPara">
         <div class="leftParaTitle">NEWS</div>
         <div class="leftParaContent">
@@ -10,8 +10,10 @@
         </div>
       </div>
     </div>
-    <div class='right'>
-        <div class='divider'></div>
+    <div class="right">
+      <img src="../assets/images/divider.png" alt="divider" />
+      <div class="link">GO TO <br />MEMORYSWAP</div>
+      <img src="../assets/images/mmcBlack.png" alt="mmc">
     </div>
   </div>
 </template>
@@ -37,24 +39,24 @@ export default {
   },
 };
 </script>
-<style lang='less' scoped>
+<style lang="less" scoped>
 .box {
   background-color: white;
   padding: 5rem 7rem;
   display: flex;
   justify-content: space-between;
+  font-style: italic;
 }
 
-.left{
-    display: flex;
-    align-items: center;
-    img{
-        height: 12rem;
-        margin-right: 1rem;
-    }
+.left {
+  display: flex;
+  align-items: center;
+  img {
+    height: 12rem;
+    margin-right: 1rem;
+  }
 }
 .leftPara {
-  width: 50%;
   margin-left: 1rem;
 }
 .leftParaTitle {
@@ -73,5 +75,18 @@ export default {
   line-height: 1.2;
   word-spacing: 0.2rem;
   letter-spacing: 0.1rem;
+}
+.right {
+  display: flex;
+  align-items: center;
+  .link {
+    cursor: pointer;
+    font-size: 3rem;
+    font-family: Arial;
+    font-weight: normal;
+    text-decoration: underline;
+    color: #009de8;
+    margin: 1rem 4rem 0rem 3rem;
+  }
 }
 </style>
--- src/components/Pledge.vue
+++ src/components/Pledge.vue
@@ -1,43 +1,127 @@
 <template>
-  <div class='box'>
-      <div class='content'></div>
+  <div class="box">
+    <div class="content">
+      <div class="left">
+        <div class="border">
+          <img src="../assets/images/mofang.png" alt="mofang" />
+        </div>
+      </div>
+      <div class="right">
+        <div class="pledgeMining">PLEDGE MINING</div>
+        <div class="subMiningContent">
+          <div class="top">
+            The ecology gradually improved and began to seekmore possibilities.
+          </div>
+          <br />
+          <br />
+          <div>
+            The ecology gradually improved and began to seekmore
+            possibilities.The ecology gradually improved and began to seekmore
+            possibilities.
+          </div>
+        </div>
+        <div class="blue">THE ECLOGY GRADUALLY 2022.01</div>
+        <div class="bottom">
+          <div class="more">MORE</div>
+          <img src="../assets/images/right.png" alt="right" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang='ts'>
-import { reactive,toRefs,onBeforeMount,onMounted} from 'vue'
+<script lang="ts">
+import { reactive, toRefs, onBeforeMount, onMounted } from "vue";
 interface DataProps {}
 export default {
-    name: '',
-      setup() {
-          console.log('1-开始创建组件-setup')
-          const data: DataProps = reactive({
-
-          })
-          onBeforeMount(() => {
-              console.log('2.组件挂载页面之前执行----onBeforeMount')
-          })
-          onMounted(() => {
-              console.log('3.-组件挂载到页面之后执行-------onMounted')
-          })
-          const refData = toRefs(data);
-          return {
-              ...refData,
-          }
-
-      }
-  };
+  name: "",
+  setup() {
+    console.log("1-开始创建组件-setup");
+    const data: DataProps = reactive({});
+    onBeforeMount(() => {
+      console.log("2.组件挂载页面之前执行----onBeforeMount");
+    });
+    onMounted(() => {
+      console.log("3.-组件挂载到页面之后执行-------onMounted");
+    });
+    const refData = toRefs(data);
+    return {
+      ...refData,
+    };
+  },
+};
 </script>
-<style  scoped>
-.box{
-    background-image: url('../assets/images/pledgeBg.png');
-    background-size: cover;
-    display: flex;
-    position: relative;    
-    height: 100vh;
+<style lang="less" scoped>
+.box {
+  width: 100%;
+  background-image: url("../assets/images/pledgeBg.png");
+  background-size: 100% 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
-.content{
-    position: absolute;
-    margin: auto;
+.content {
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  .left {
+    width: 50%;
+    display: flex;
+    align-items: center;
+    .border {
+      width: fit-content;
+      display: flex;
+      justify-items: center;
+      align-items: center;
+      padding: 10rem 8rem;
+      border: 5px solid white;
+    }
+  }
+  .right {
+    width: 50%;
+    .pledgeMining {
+      width: 100%;
+      text-align: center;
+      border: 9px solid white;
+      padding: 0.5rem 1rem;
+      font-size: 4.5rem;
+      font-weight: 900;
+      color: #ffffff;
+      margin-bottom: 2rem;
+    }
+    .subMiningContent {
+      font-size: 1.5rem;
+      font-weight: normal;
+      font-style: italic;
+      color: #ffffff;
+      margin-bottom: 3rem;
+    }
+  }
+  .blue {
+    font-size: 2rem;
+    font-weight: bold;
+    color: #0091c9;
+    line-height: 1.5rem;
+    margin-bottom: 6rem;
+  }
+  .bottom {
+    display: flex;
+    flex-direction: row-reverse;
+    cursor: pointer;
+    img{
+        margin-right: 2rem;
+    }
+    .more {
+      border: 10px solid white;
+      padding: 0.5rem 0;
+      width: 50%;
+      text-align: center;
+      font-size: 2rem;
+      font-family: zihun143-zhengkuchaojihei;
+      font-weight: bold;
+      color: #ffffff;
+    }
+  }
 }
 </style>
